5496fca3124d4074576c36da2f20e11d08391a3f
anirbanbhunia <anirbanbhunia7@gmail.com>
2024-03-31 19:11:34 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import Login from "./Components/Login";
+import Profile from "./Components/Profile";
+import UserContextProvider from "./Context/UserContextProvider";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <UserContextProvider>
+      <Login/>
+      <Profile/>
+    </UserContextProvider>
   );
 }
 
